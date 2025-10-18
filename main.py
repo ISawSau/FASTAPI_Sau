bab3aee6bbd0af2548d943947d2affd3bd84d306
ISawSau <joansaulopez@gmail.com>
2025-10-18 19:37:55 +0200

    acabat (sense el patch)

--- main.py
+++ main.py
@@ -6,23 +6,28 @@ users = []
 
 @app.post("/api/users", response_model = dict)
 async def createUser():
-    #
+    newUser = {"id": len(users) + 1, "name": "nomUsuari", "email": "email@iticbcn.cat"}
+    users.append(newUser)
     return{"users": users}
 
 @app.get("/api/users/{id}", response_model = dict)
 async def readUserById(id: int):
-    #
-    return{"userData": "dades de l'usuari"}
+    for user in users:
+        if user["id"] == id:
+            return{"user": user}
+    return{"user": None}
 
 @app.get("/api/users", response_model = dict)
 async def readAllUsers():
-    #
     return{"users": users}
 
-@app.put("api/users/{id}", response_model = dict)
+@app.put("/api/users/{id}", response_model = dict)
 async def updateUser(id: int):
-    #
-    return{"users": "usuari actualizat"}
+    for user in users:
+        if user["id"] == id:
+            user["name"] = "nomActualitzat"
+            user["email"] = "emailNou@iticbcn.cat"
+    return{"users": users}
 
 """
 @app.patch("api/users/{id}", response_model = dict)
@@ -31,7 +36,12 @@ async def partialUserUpdate(id : int):
     return{"users": "usuari parcialment actualitzat"}
 """
 
-@app.delete("api/users/{id}", response_model = dict)
+@app.delete("/api/users/{id}", response_model = dict)
 async def deleteUser(id: int):
-    #
+    userFound = None
+    for user in users:
+        if user["id"] == id:
+            userFound = user
+    if userFound:
+        users.remove(userFound)
     return{"users": users}
